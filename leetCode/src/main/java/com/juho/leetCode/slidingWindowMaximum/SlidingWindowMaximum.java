e3c580d6469c5183ba4e42f38bc71a7c6c3815af
juho0719 <juho0719@wemakeprice.com>
2020-09-01 18:35:01 +0900

    # leetCode - sliding window maximum (brute-force)
    - brute-force complete

--- leetCode/src/main/java/com/juho/leetCode/slidingWindowMaximum/SlidingWindowMaximum.java
+++ leetCode/src/main/java/com/juho/leetCode/slidingWindowMaximum/SlidingWindowMaximum.java
@@ -1,11 +1,33 @@
 package com.juho.leetCode.slidingWindowMaximum;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class SlidingWindowMaximum {
 	public int[] bruteForceMax(int[] nums, int k) {
-		int numOfWindows = nums.length - k + 1
+		int numOfWindows = nums.length - k + 1;
+		int[] result = new int[numOfWindows];
+		int max = -10 ^ 4;
+
+		for (int i = 0; i < numOfWindows; i++) {
+			for (int j = i; j < k + i - 1; j++) {
+				if (j == i)
+					max = nums[j];
+				if (max < nums[j + 1]) {
+					max = nums[j + 1];
+				}
+			}
+			result[i] = max;
+		}
+
+		return result;
+	}
+
+	public int[] dequeMax(int[] nums, int k) {
+		int numOfWindows = nums.length - k + 1;
 		int[] result = new int[numOfWindows];
 
-		
+		Deque<Integer> deque = new ArrayDeque<>();
 
 		return result;
 	}
--- leetCode/src/test/java/com/juho/leetCode/slidingWindowMaximum/SlidingWindowMaximumTest.java
+++ leetCode/src/test/java/com/juho/leetCode/slidingWindowMaximum/SlidingWindowMaximumTest.java
@@ -1,6 +1,7 @@
 package com.juho.leetCode.slidingWindowMaximum;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,18 +12,39 @@ public class SlidingWindowMaximumTest {
 	@Test
 	public void testBruteForceMax() {
 		int[] nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
-		int k = 3;
-		int[] result = { 3, 3, 5, 5, 6, 7 };
+		int[] nums2 = { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+		int[] nums3 = { 8, 5, 10, 7, 9, 4, 15, 12, 90, 13 };
 
-		assertEquals(result, swm.bruteForceMax(nums, k));
+		int k = 3;
+		int k2 = 3;
+		int k3 = 4;
+
+		int[] result = { 3, 3, 5, 5, 6, 7 };
+		int[] result2 = { 3, 3, 4, 5, 5, 5, 6 };
+		int[] result3 = { 10, 10, 10, 15, 15, 90, 90 };
+
+		assertThat(result).containsOnly(swm.bruteForceMax(nums, k));
+		assertThat(result2).containsOnly(swm.bruteForceMax(nums2, k2));
+		assertThat(result3).containsOnly(swm.bruteForceMax(nums3, k3));
+		// assertEquals(result, swm.bruteForceMax(nums, k));
 	}
 
 	@Test
 	public void testDequeMax() {
 		int[] nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
-		int k = 3;
-		int[] result = { 3, 3, 5, 5, 6, 7 };
+		int[] nums2 = { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+		int[] nums3 = { 8, 5, 10, 7, 9, 4, 15, 12, 90, 13 };
 
-		assertEquals(result, swm.bruteForceMax(nums, k));
+		int k = 3;
+		int k2 = 3;
+		int k3 = 4;
+
+		int[] result = { 3, 3, 5, 5, 6, 7 };
+		int[] result2 = { 3, 3, 4, 5, 5, 5, 6 };
+		int[] result3 = { 10, 10, 10, 15, 15, 90, 90 };
+
+		assertThat(result).containsOnly(swm.dequeMax(nums, k));
+		assertThat(result2).containsOnly(swm.dequeMax(nums2, k2));
+		assertThat(result3).containsOnly(swm.dequeMax(nums3, k3));
 	}
 }
