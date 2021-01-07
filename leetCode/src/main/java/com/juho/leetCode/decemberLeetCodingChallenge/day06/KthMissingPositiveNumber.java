b2bebf437c870a29b004946ce104ad1b583c132c
juho0719 <juho0719@naver.com>
2021-01-07 16:05:00 +0900

    December LeetCode Challenge
    - day06 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/decemberLeetCodingChallenge/day06/KthMissingPositiveNumber.java
@@ -0,0 +1,29 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day06;
+
+public class KthMissingPositiveNumber {
+    
+    public int findKthPositive(int[] arr, int k) {
+        int count = 0;
+        int arrIdx = 0;
+        int i=0;
+        while(count!=k) {
+            if(arrIdx == arr.length) {
+                return i+(k-count);
+            }
+
+            i++;
+
+            if(arr[arrIdx] == i) {
+                arrIdx++;
+                continue;
+            } else {
+                count++;
+            }
+            if(count == k) {
+                return i;
+            }
+        }
+        return i;
+    }
+
+}
