7e4212be3f1e112b2e767ad77ca32d85b939736e
juho0719 <juho0719@naver.com>
2021-01-16 01:43:39 +0900

    December LeetCode Challenge
    - day15 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/decemberLeetCodeingChallenge/day15/GetMaximumInGeneratedArrayTest.java
@@ -0,0 +1,17 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day15;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class GetMaximumInGeneratedArrayTest {
+
+    GetMaximumInGeneratedArray gmiga = new GetMaximumInGeneratedArray();
+
+    @Test
+    public void minimumOperationsToReduceXtoZero01Test() {
+        assertEquals(gmiga.getMaximumGenerated(7), 3);
+        assertEquals(gmiga.getMaximumGenerated(2), 1);
+        assertEquals(gmiga.getMaximumGenerated(3), 2);
+    }
+}
