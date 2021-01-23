fa2d2303abec6f87d3753344931136f3793b4844
juho0719 <juho0719@naver.com>
2021-01-24 01:51:02 +0900

    December LeetCode Challenge
    - day22 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/decemberLeetCodingChallenge/day22/DetermineIfTwoStringsAreCloseTest.java
@@ -0,0 +1,28 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day22;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class DetermineIfTwoStringsAreCloseTest {
+
+    DetermineIfTwoStringsAreClose ditsac = new DetermineIfTwoStringsAreClose();
+
+    @Test
+    public void determineIfTwoStringsAreClose01Test() {
+        String word01 = "abc";
+        String word02 = "bca";
+        assertEquals(ditsac.closeStrings(word01, word02), true);
+
+        String word11 = "a";
+        String word12 = "aa";
+        assertEquals(ditsac.closeStrings(word11, word12), false);
+
+        String word21 = "cabbba";
+        String word22 = "abbccc";
+        assertEquals(ditsac.closeStrings(word21, word22), true);
+
+        String word31 = "cabbba";
+        String word32 = "aabbss";
+        assertEquals(ditsac.closeStrings(word31, word32), false);
+    }
+}
