2150d45289f8b8a0ff262c9dbd1efb15e186a646
juho0719 <juho0719@naver.com>
2021-01-24 15:22:40 +0900

    December LeetCode Challenge
    - day23 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/decemberLeetCodingChallenge/day23/SortTheMatrixDiagonallyTest.java
@@ -0,0 +1,16 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day23;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class SortTheMatrixDiagonallyTest {
+
+    SortTheMatrixDiagonally stmd = new SortTheMatrixDiagonally();
+
+    @Test
+    public void determineIfTwoStringsAreClose01Test() {
+        int[][] mat01 = {{3,3,1,1}, {2,2,1,2}, {1,1,1,2}};
+        int[][] result01 = {{1,1,1,1}, {1,2,2,2}, {1,2,3,3}};
+        assertArrayEquals(stmd.diagonalSort(mat01), result01);
+    }
+}
