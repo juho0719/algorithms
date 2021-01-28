77374ed3a0bb12e8a544d685f299dda50c14a976
juho0719 <juho0719@naver.com>
2021-01-29 07:28:17 +0900

    December LeetCode Challenge
    - day28 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/decemberLeetCodingChallenge/day28/SmallestStringWithAGivenNumericValue.java
@@ -0,0 +1,22 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day28;
+
+import java.util.Arrays;
+
+public class SmallestStringWithAGivenNumericValue {
+
+    public String getSmallestString(int n, int k) {
+        
+        char[] ch = new char[n];
+        Arrays.fill(ch, 'a');
+        k -= n;
+        n--;
+        
+        while(k >= 26) {
+            ch[n--] = 'z';
+            k = k + 1 - 26;
+        }
+        ch[n] += k;
+
+        return String.valueOf(ch);
+    }
+}
