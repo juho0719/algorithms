f841294a512e1317740877418e9ce80d620c8a4b
juho0719 <juho0719@naver.com>
2021-01-28 17:54:10 +0900

    December LeetCode Challenge
    - day26 finished
    - day27 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/decemberLeetCodingChallenge/day27/ConcatenationOfConsecutiveBinaryNumbers.java
@@ -0,0 +1,14 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day27;
+
+public class ConcatenationOfConsecutiveBinaryNumbers {
+
+    public int concatenatedBinary(int n) {
+        int digits = 0, MOD = 1000000007;
+        long result = 0;
+        for(int i = 1; i <= n; i++){
+            if((i & (i - 1)) == 0) digits++; 
+            result = ((result << digits) + i) % MOD;
+        }
+        return (int) result;
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/decemberLeetCodingChallenge/day27/ConcatenationOfConsecutiveBinaryNumbersTest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day27;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ConcatenationOfConsecutiveBinaryNumbersTest {
+
+    ConcatenationOfConsecutiveBinaryNumbers cocbn = new ConcatenationOfConsecutiveBinaryNumbers();
+
+    @Test
+    public void concatenationOfConsecutiveBinaryNumbers01Test() {
+        int n01 = 1;
+        assertEquals(cocbn.concatenatedBinary(n01), 1);
+
+        int n02 = 3;
+        assertEquals(cocbn.concatenatedBinary(n02), 27);
+
+        int n03 = 12;
+        assertEquals(cocbn.concatenatedBinary(n03), 505379714);
+    }
+}
