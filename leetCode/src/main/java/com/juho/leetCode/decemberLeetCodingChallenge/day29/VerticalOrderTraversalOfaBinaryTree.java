ef2ac735333cba9ff62a8668de18065f8462ecfe
juho0719 <juho0719@naver.com>
2021-01-30 01:35:51 +0900

    December LeetCode Challenge
    - day29 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/decemberLeetCodingChallenge/day29/VerticalOrderTraversalOfaBinaryTree.java
@@ -0,0 +1,50 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day29;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+import com.juho.leetCode.decemberLeetCodingChallenge.datastructure.TreeNode2;
+
+public class VerticalOrderTraversalOfaBinaryTree {
+
+    public List<List<Integer>> verticalTraversal(TreeNode2 root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return null;
+
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        binaryTree(root, 0, 0, map);
+        
+        for(Map.Entry<Integer, TreeMap<Integer, PriorityQueue<Integer>>> entry : map.entrySet()) {
+            List<Integer> line = new ArrayList<>();
+            for (PriorityQueue<Integer> pq : entry.getValue().values()) {
+                while(pq.size() > 0) {
+                    line.add(pq.poll());
+                }
+            }
+            result.add(line);
+        }
+        return result;
+    }
+
+    private void binaryTree(TreeNode2 root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+        if(root == null) return;
+
+        TreeMap<Integer, PriorityQueue<Integer>> bMap = new TreeMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        if(map.containsKey(x)) {
+            bMap = map.get(x);
+            if(bMap.containsKey(y)) {
+                pq = bMap.get(y);
+            }
+        }
+        pq.add(root.val);
+        bMap.put(y, pq);
+        map.put(x, bMap);
+
+        binaryTree(root.left, x - 1, y + 1, map);
+        binaryTree(root.right, x + 1, y + 1, map);
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/decemberLeetCodingChallenge/day29/VerticalOrderTraversalOfaBinaryTreeTest.java
@@ -0,0 +1,35 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day29;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+import com.juho.leetCode.decemberLeetCodingChallenge.datastructure.TreeNode2;
+
+import org.junit.jupiter.api.Test;
+
+public class VerticalOrderTraversalOfaBinaryTreeTest {
+
+    VerticalOrderTraversalOfaBinaryTree votobt = new VerticalOrderTraversalOfaBinaryTree();
+
+    @Test
+    public void smallestStringWithAGivenNumericValue01Test() {
+        TreeNode2 root01 = new TreeNode2(3, new TreeNode2(9), new TreeNode2(20, new TreeNode2(15), new TreeNode2(7)));
+        
+        List<List<Integer>> vResult01 = new ArrayList<>();
+        List<Integer> vResult01Detail01 = new ArrayList<>();
+        vResult01Detail01.add(9);
+        vResult01.add(vResult01Detail01);
+        List<Integer> vResult01Detail02 = new ArrayList<>();
+        vResult01Detail02.add(3);
+        vResult01Detail02.add(15);
+        vResult01.add(vResult01Detail02);
+        List<Integer> vResult01Detail03 = new ArrayList<>();
+        vResult01Detail03.add(20);
+        vResult01.add(vResult01Detail03);
+        List<Integer> vResult01Detail04 = new ArrayList<>();
+        vResult01Detail04.add(7);
+        vResult01.add(vResult01Detail04);
+
+        assertEquals(votobt.verticalTraversal(root01), vResult01);
+    }
+}
