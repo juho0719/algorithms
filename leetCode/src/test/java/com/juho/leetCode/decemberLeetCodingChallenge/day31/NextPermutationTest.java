c3ef410bc319b50d317db1cdc7113449d0dd093e
juho0719 <juho0719@naver.com>
2021-02-01 00:23:21 +0900

    December LeetCode Challenge
    - day31 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/decemberLeetCodingChallenge/day31/NextPermutationTest.java
@@ -0,0 +1,40 @@
+package com.juho.leetCode.decemberLeetCodingChallenge.day31;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class NextPermutationTest {
+
+    NextPermutation np = new NextPermutation();
+
+    @Test
+    public void nextPermutation01Test() {
+        int[] nums01 = {1,2,3};
+        np.nextPermutation(nums01);
+        int[] result01 = {1,3,2};
+        assertArrayEquals(nums01, result01);
+
+        int[] nums02 = {3,2,1};
+        np.nextPermutation(nums02);
+        int[] result02 = {1,2,3};
+        assertArrayEquals(nums02, result02);
+
+        int[] nums03 = {1,1,5};
+        np.nextPermutation(nums03);
+        int[] result03 = {1,5,1};
+        assertArrayEquals(nums03, result03);
+
+        int[] nums04 = {1};
+        np.nextPermutation(nums04);
+        int[] result04 = {1};
+        assertArrayEquals(nums04, result04);
+
+        int[] nums05 = {1,3,2};
+        np.nextPermutation(nums05);
+        int[] result05 = {2,1,3};
+        assertArrayEquals(nums05, result05);
+    }
+}
