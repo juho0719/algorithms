5097f48a62a058eba194de50316c91e4ccc70fdc
juho0719 <juho0719@naver.com>
2021-02-15 23:24:13 +0900

    FebruaryLeetCodingChallenge
    - day13 finished
    - day14 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/februaryLeetCodingChallenge/day14/IsGraphBipartite.java
@@ -0,0 +1,55 @@
+package com.juho.leetCode.februaryLeetCodingChallenge.day14;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsGraphBipartite {
+
+    public boolean isBipartiteByBFS(int[][] graph) {
+        int N = graph.length;
+        int[] colors = new int[N]; // 0 not colored, 1 = Red, -1 = Blue
+        for(int vertex = 0; vertex < N; vertex++) { // handle forest
+            if(colors[vertex] == 0) {
+                colors[vertex] = 1;
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(vertex);
+                
+                while(!queue.isEmpty()) {
+                    int currVert = queue.poll();
+                    for(int adjVert : graph[currVert]) {
+                        if(colors[adjVert] == colors[currVert])
+                            return false;
+                        if(colors[adjVert] == 0) {
+                            colors[adjVert] = -colors[currVert];
+                            queue.add(adjVert);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+
+    public boolean isBipartiteByDFS(int[][] graph) {
+        int N = graph.length;
+        int[] colors = new int[N]; // 0 not colored, 1 = Red, -1 = Blue
+        for(int vertex = 0; vertex < N; vertex++)
+            if(colors[vertex] == 0 && !dfs(graph, vertex, 1, colors))
+                return false;
+        
+        return true;
+    }
+    
+    private boolean dfs(int[][] graph, int vertex, int color, int[] colors) {
+        if(colors[vertex] != 0)
+            return colors[vertex] == color;
+            
+        colors[vertex] = color;
+        for(int adjVert : graph[vertex])
+            if(!dfs(graph, adjVert, -color, colors)) 
+                return false;
+        
+        return true;
+    }
+}
