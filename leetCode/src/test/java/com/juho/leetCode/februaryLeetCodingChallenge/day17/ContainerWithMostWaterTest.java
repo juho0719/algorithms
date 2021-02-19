94ba65cb6e1947e126d0d6744f00895c960497a5
juho0719 <juho0719@naver.com>
2021-02-19 19:34:54 +0900

    FebruaryLeetCodingChallenge
    - day17 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/februaryLeetCodingChallenge/day17/ContainerWithMostWaterTest.java
@@ -0,0 +1,25 @@
+package com.juho.leetCode.februaryLeetCodingChallenge.day17;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ContainerWithMostWaterTest {
+    
+    ContainerWithMostWater cwmw = new ContainerWithMostWater();
+
+    @Test
+    public void containerWithMostWater01Test() {
+        int[] height01 = {1,8,6,2,5,4,8,3,7};
+        assertEquals(cwmw.maxArea(height01), 49);
+
+        int[] height02 = {1,1};
+        assertEquals(cwmw.maxArea(height02), 1);
+
+        int[] height03 = {4,3,2,1,4};
+        assertEquals(cwmw.maxArea(height03), 16);
+
+        int[] height04 = {1,2,1};
+        assertEquals(cwmw.maxArea(height04), 2);
+    }
+}
