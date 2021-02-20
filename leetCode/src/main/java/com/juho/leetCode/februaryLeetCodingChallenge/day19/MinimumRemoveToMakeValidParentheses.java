3aaaa82909a10eecf0dd56ba75d710f534294c99
juho0719 <juho0719@naver.com>
2021-02-21 02:44:51 +0900

    FebruaryLeetCodingChallenge
    - day18 finished
    - day19 finished
    - day20 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/februaryLeetCodingChallenge/day19/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,33 @@
+package com.juho.leetCode.februaryLeetCodingChallenge.day19;
+
+import java.util.Stack;
+
+public class MinimumRemoveToMakeValidParentheses {
+
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> openQuates = new Stack<>();
+        int continueCount = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                openQuates.add(i);
+            } else if(c == ')') {
+                if(openQuates.isEmpty()) {
+                    continueCount++;
+                    continue;
+                }
+                openQuates.pop();
+            }
+            sb.append(c);
+        }
+
+        while(!openQuates.isEmpty()) {
+            int index = openQuates.pop();
+            sb.deleteCharAt(index-continueCount);
+        }
+
+        return sb.toString();
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/februaryLeetCodingChallenge/day20/RomanToIntegerTest.java
@@ -0,0 +1,27 @@
+package com.juho.leetCode.februaryLeetCodingChallenge.day20;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class RomanToIntegerTest {
+    
+    RomanToInteger ri = new RomanToInteger();
+
+    @Test
+    public void minimumRemoveToMakeValidParentheses01Test() {
+        String s01 = "III";
+        assertEquals(ri.romanToInt(s01), 3);
+
+        String s02 = "IV";
+        assertEquals(ri.romanToInt(s02), 4);
+
+        String s03 = "IX";
+        assertEquals(ri.romanToInt(s03), 9);
+
+        String s04 = "LVIII";
+        assertEquals(ri.romanToInt(s04), 58);
+
+        String s05 = "MCMXCIV";
+        assertEquals(ri.romanToInt(s05), 1994);
+    }
+}
