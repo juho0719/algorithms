ae55c7d83d271838598a36f1988b2b0ef5e80284
juho0719 <juho0719@naver.com>
2021-02-27 02:40:27 +0900

    FebruaryLeetCodingChallenge
    - day24 finished
    - day25 finished
    - day26 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/februaryLeetCodingChallenge/day26/ValidateStackSequencesTest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.februaryLeetCodingChallenge.day26;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidateStackSequencesTest {
+    
+    ValidateStackSequences vss = new ValidateStackSequences();
+
+    @Test
+    public void shortestUnsortedContinuousSubarray01Test() {
+        int[] pushed01 = {1,2,3,4,5};
+        int[] popped01 = {4,5,3,2,1};
+        assertEquals(vss.validateStackSequences(pushed01, popped01), true);
+        
+        int[] pushed02 = {1,2,3,4,5};
+        int[] popped02 = {4,3,5,1,2};
+        assertEquals(vss.validateStackSequences(pushed02, popped02), false);
+    }
+}
