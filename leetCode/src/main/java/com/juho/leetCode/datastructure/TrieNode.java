d04f65407d54697afd4775edf32e3f2072093126
juho0719 <juho0719@naver.com>
2021-03-07 15:09:11 +0900

    MarchLeetCodingChallenge
    - day06 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/datastructure/TrieNode.java
@@ -0,0 +1,17 @@
+package com.juho.leetCode.datastructure;
+
+public class TrieNode {
+    TrieNode[] children;
+    public int count;
+    public TrieNode() {
+        children = new TrieNode[26];
+        count = 0;
+    }
+    public TrieNode get(char c) {
+        if (children[c-'a'] == null) {
+            children[c-'a'] = new TrieNode();
+            count++;
+        }
+        return children[c - 'a'];
+    }
+}
