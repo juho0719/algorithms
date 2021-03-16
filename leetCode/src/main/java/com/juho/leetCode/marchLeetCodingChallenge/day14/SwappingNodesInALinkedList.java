bd2cdf7ef346a5082b61d28eaa2f61c92f02429d
juho0719 <juho0719@naver.com>
2021-03-16 15:13:08 +0900

    MarchLeetCodingChallenge
    - day14 finished
    - day15 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/marchLeetCodingChallenge/day14/SwappingNodesInALinkedList.java
@@ -0,0 +1,27 @@
+package com.juho.leetCode.marchLeetCodingChallenge.day14;
+
+import com.juho.leetCode.datastructure.ListNode;
+
+public class SwappingNodesInALinkedList {
+    public ListNode swapNodes(ListNode head, int k) {
+        k = k-1;
+        ListNode firstN = head;
+        while(k>0){
+            firstN = firstN.next;
+            k--;
+        }
+
+        ListNode f = firstN;
+        ListNode secondN = head;
+        while(f.next!=null){
+            secondN = secondN.next;
+            f = f.next;
+        }
+
+        int temp = firstN.val;
+        firstN.val = secondN.val;
+        secondN.val = temp;
+
+        return head;
+    }
+}
