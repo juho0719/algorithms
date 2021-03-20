7ea0d5489b352f1316413efbf0ea4bbf832b0ade
juho0719 <juho0719@naver.com>
2021-03-20 17:29:24 +0900

    MarchLeetCodingChallenge
    - day19 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/marchLeetCodingChallenge/day19/KeysAndRoomsTest.java
@@ -0,0 +1,28 @@
+package com.juho.leetCode.marchLeetCodingChallenge.day19;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class KeysAndRoomsTest {
+    
+    KeysAndRooms kr = new KeysAndRooms();
+
+    @Test
+    public void wiggleSubsequence01Test() {
+        List<List<Integer>> rooms01 = new ArrayList<>();
+        List<Integer> rooms01Sub01 = new ArrayList<>();
+        List<Integer> rooms01Sub02 = new ArrayList<>();
+        List<Integer> rooms01Sub03 = new ArrayList<>();
+        List<Integer> rooms01Sub04 = new ArrayList<>();
+        rooms01Sub01.add(1);
+        rooms01Sub02.add(2);
+        rooms01Sub03.add(3);
+        rooms01.add(rooms01Sub01);
+        rooms01.add(rooms01Sub02);
+        rooms01.add(rooms01Sub03);
+        rooms01.add(rooms01Sub04);
+        assertEquals(true, kr.canVisitAllRooms(rooms01));
+    }
+}
