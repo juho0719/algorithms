86213a119b8369af4187e25abf766129da1e9213
juho0719 <juho0719@naver.com>
2021-03-21 15:06:19 +0900

    MarchLeetCodingChallenge
    - day20 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/marchLeetCodingChallenge/day20/UndergroundSystemTest.java
@@ -0,0 +1,33 @@
+package com.juho.leetCode.marchLeetCodingChallenge.day20;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class UndergroundSystemTest {
+    
+    UndergroundSystem us = new UndergroundSystem();
+
+    @Test
+    public void undergroundSystem01Test() {
+        us.checkIn(45, "Leyton", 3);
+        us.checkIn(32, "Paradise", 8);
+        us.checkIn(27, "Leyton", 10);
+        us.checkOut(45, "Waterloo", 15);
+        us.checkOut(27, "Waterloo", 20);
+        us.checkOut(32, "Cambridge", 22);
+        double avg01 = 14;
+        assertEquals(avg01, us.getAverageTime("Paradise", "Cambridge"));
+        double avg02 = 11;
+        assertEquals(avg02, us.getAverageTime("Leyton", "Waterloo"));
+        
+        us.checkIn(10, "Leyton", 24);
+
+        double avg03 = 11;
+        assertEquals(avg03, us.getAverageTime("Leyton", "Waterloo"));
+        
+        us.checkOut(10, "Waterloo", 38);
+
+        double avg04 = 12;
+        assertEquals(avg04, us.getAverageTime("Leyton", "Waterloo"));
+    }
+}
