f21369d46abb64385154782c4752c37ceb22873b
juho0719 <juho0719@naver.com>
2021-03-25 13:11:08 +0900

    MarchLeetCodingChallenge
    - day22 finished
    - day23 finished
    - day24 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/marchLeetCodingChallenge/day24/AdvantageShuffle.java
@@ -0,0 +1,9 @@
+package com.juho.leetCode.marchLeetCodingChallenge.day24;
+
+public class AdvantageShuffle {
+    
+    public int[] advantageCount(int[] A, int[] B) {
+        
+        return A;
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/marchLeetCodingChallenge/day24/AdvantageShuffleTest.java
@@ -0,0 +1,22 @@
+package com.juho.leetCode.marchLeetCodingChallenge.day24;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class AdvantageShuffleTest {
+    
+    AdvantageShuffle as = new AdvantageShuffle();
+
+    @Test
+    public void advantageShuffle01Test() {
+        int[] A01 = {2,7,11,15};
+        int[] B01 = {1,10,4,11};
+        int[] result01 = {2,11,7,15};
+        assertArrayEquals(result01, as.advantageCount(A01, B01));
+
+        int[] A02 = {12,24,8,32};
+        int[] B02 = {13,25,32,11};
+        int[] result02 = {24,32,8,12};
+        assertArrayEquals(result02, as.advantageCount(A02, B02));
+    }
+}
