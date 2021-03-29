fb386c308088cf77f2a1043229b5c4213e927f80
juho0719 <juho0719@naver.com>
2021-03-29 23:03:46 +0900

    MarchLeetCodingChallenge
    - day27 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/marchLeetCodingChallenge/day27/PalindromicSubstringsTest.java
@@ -0,0 +1,19 @@
+package com.juho.leetCode.marchLeetCodingChallenge.day27;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PalindromicSubstringsTest {
+    
+    PalindromicSubstrings ps = new PalindromicSubstrings();
+
+    @Test
+    public void palindromicSubstrings01Test() {
+        String s01 = "abc";
+        assertEquals(3, ps.countSubstrings(s01));
+
+        String s02 = "aaa";
+        assertEquals(6, ps.countSubstrings(s02));
+    }
+}
