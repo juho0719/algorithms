bb259e94a85fee51e0395fc1faabaca50d24b1aa
juho0719 <juho0719@naver.com>
2021-04-04 15:21:59 +0900

    AprilLeetCodingChallenge
    - day01 finished
    - day02 finished
    - day03 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/aprilLeetCodingChallenge/day03/LongestValidParenthesesTest.java
@@ -0,0 +1,24 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class LongestValidParenthesesTest {
+    
+    LongestValidParentheses lvp = new LongestValidParentheses();
+
+    @Test
+    public void longestValidParentheses01Test() {
+        String s01 = "(()";
+        assertEquals(2, lvp.longestValidParentheses(s01));
+
+        String s02 = ")()())";
+        assertEquals(4, lvp.longestValidParentheses(s02));
+
+        String s03 = "";
+        assertEquals(0, lvp.longestValidParentheses(s03));
+
+        String s04 = "()(()";
+        assertEquals(2, lvp.longestValidParentheses(s04));
+    }
+}
