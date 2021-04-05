7fa877afd3d60da25351333661ee8a8060ac5b4d
juho0719 <juho0719@naver.com>
2021-04-05 23:52:20 +0900

    AprilLeetCodingChallenge
    - day04 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/aprilLeetCodingChallenge/day04/DesignCircularQueueTest.java
@@ -0,0 +1,22 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day04;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class DesignCircularQueueTest {
+
+    @Test
+    public void designCircularQueue01Test() {
+        DesignCircularQueue dcq = new DesignCircularQueue(3);
+        
+        assertEquals(true, dcq.enQueue(1));
+        assertEquals(true, dcq.enQueue(2));
+        assertEquals(true, dcq.enQueue(3));
+        assertEquals(false, dcq.enQueue(4));
+        assertEquals(3, dcq.Rear());
+        assertEquals(true, dcq.isFull());
+        assertEquals(true, dcq.deQueue());
+        assertEquals(true, dcq.enQueue(4));
+        assertEquals(4, dcq.Rear());
+    }
+}
