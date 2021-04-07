02ee90f49f570bd27d5a1b0d31b54db4cafd0934
juho0719 <juho0719@naver.com>
2021-04-07 22:35:57 +0900

    AprilLeetCodingChallenge
    - day06 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/aprilLeetCodingChallenge/day06/MinimumOperationsToMakeArrayEqual.java
@@ -0,0 +1,8 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day06;
+
+public class MinimumOperationsToMakeArrayEqual {
+
+    public int minOperations(int n) {
+        return n * n / 4;
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/aprilLeetCodingChallenge/day06/MinimumOperationsToMakeArrayEqualTest.java
@@ -0,0 +1,18 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day06;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MinimumOperationsToMakeArrayEqualTest {
+
+    MinimumOperationsToMakeArrayEqual momae = new MinimumOperationsToMakeArrayEqual();
+
+    @Test
+    public void minimumOperationsToMakeArrayEqual01Test() {
+        int n01 = 3;
+        assertEquals(2, momae.minOperations(n01));
+
+        int n02 = 6;
+        assertEquals(9, momae.minOperations(n02));
+    }
+}
