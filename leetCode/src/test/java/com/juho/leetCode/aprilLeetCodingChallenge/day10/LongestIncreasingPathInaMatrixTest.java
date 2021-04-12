5051320d79cfd42107510edcdd6b93dab518765f
juho0719 <juho0719@naver.com>
2021-04-12 14:31:33 +0900

    AprilLeetCodingChallenge
    - day10 finished
    - day11 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/aprilLeetCodingChallenge/day10/LongestIncreasingPathInaMatrixTest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day10;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class LongestIncreasingPathInaMatrixTest {
+
+    LongestIncreasingPathInaMatrix lipm = new LongestIncreasingPathInaMatrix();
+
+    @Test
+    public void longestIncreasingPathInaMatrix01Test() {
+        int[][] matrix01 = {{9,9,4}, {6,6,8}, {2,1,1}};
+        assertEquals(4, lipm.longestIncreasingPath(matrix01));
+
+        int[][] matrix02 = {{3,4,5}, {3,2,6}, {2,2,1}};
+        assertEquals(4, lipm.longestIncreasingPath(matrix02));
+
+        int[][] matrix03 = {{1}};
+        assertEquals(1, lipm.longestIncreasingPath(matrix03));
+    }
+}
