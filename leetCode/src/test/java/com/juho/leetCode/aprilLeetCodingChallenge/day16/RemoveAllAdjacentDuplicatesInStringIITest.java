84b504fd95454134902960db50ce8480d3abb7ca
juho0719 <juho0719@naver.com>
2021-04-17 18:45:04 +0900

    AprilLeetCodingChallenge
    - day16 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/aprilLeetCodingChallenge/day16/RemoveAllAdjacentDuplicatesInStringIITest.java
@@ -0,0 +1,16 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day16;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class RemoveAllAdjacentDuplicatesInStringIITest {
+
+    RemoveAllAdjacentDuplicatesInStringII rads2 = new RemoveAllAdjacentDuplicatesInStringII();
+
+    @Test
+    public void RemoveAllAdjacentDuplicatesInStringII01Test() {
+        assertEquals("abcd", rads2.removeDuplicates("abcd", 2));
+        assertEquals("aa", rads2.removeDuplicates("deeedbbcccbdaa", 3));
+        assertEquals("ps", rads2.removeDuplicates("pbbcggttciiippooaais", 2));
+    }
+}
