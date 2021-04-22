56e55c4df9817421c8d3cf6df3c1c29ef1138afa
juho0719 <juho0719@naver.com>
2021-04-22 23:47:10 +0900

    AprilLeetCodingChallenge
    - day21 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/aprilLeetCodingChallenge/day21/TriangleTest.java
@@ -0,0 +1,14 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day21;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+
+    Triangle t = new Triangle();
+
+    @Test
+    public void naryTreePreorderTraversal01Test() {
+        
+    }
+}
