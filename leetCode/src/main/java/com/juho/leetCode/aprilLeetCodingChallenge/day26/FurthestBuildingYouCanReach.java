01d897d7369ed0cadba79cc7dbdfbe79c6387cc0
juho0719 <juho0719@naver.com>
2021-04-27 23:34:21 +0900

    AprilLeetCodingChallenge
    - day26 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/aprilLeetCodingChallenge/day26/FurthestBuildingYouCanReach.java
@@ -0,0 +1,24 @@
+package com.juho.leetCode.aprilLeetCodingChallenge.day26;
+
+import java.util.PriorityQueue;
+
+public class FurthestBuildingYouCanReach {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        int len = heights.length - 1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < len; i++) {
+            int diff = heights[i+1] - heights[i];
+            if (diff > 0) {
+                if (ladders > 0) {
+                    pq.add(diff);
+                    ladders--;
+                } else if (pq.size() > 0 && diff > pq.peek()) {
+                    pq.add(diff);
+                    bricks -= pq.poll();
+                } else bricks -= diff;
+                if (bricks < 0) return i;
+            }
+        }
+        return len;
+    }
+}
