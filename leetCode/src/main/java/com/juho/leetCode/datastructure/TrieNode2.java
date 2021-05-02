c2b591ae89151de1047cc23ba9c9706f181c0162
juho0719 <juho0719@naver.com>
2021-05-02 15:47:20 +0900

    MayLeetCodingChallenge
    - day01 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/datastructure/TrieNode2.java
@@ -0,0 +1,9 @@
+package com.juho.leetCode.datastructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrieNode2 {
+    public TrieNode2[] children = new TrieNode2[26];
+    public List<Integer> vals = new ArrayList<>();
+}
