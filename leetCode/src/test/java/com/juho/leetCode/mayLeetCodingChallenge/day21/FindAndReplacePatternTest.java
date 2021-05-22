c01c1abca23642f055edc7441f1237b2f16671a1
juho0719 <juho0719@naver.com>
2021-05-22 13:03:43 +0900

    MayLeetCodingChallenge
    - day21 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/mayLeetCodingChallenge/day21/FindAndReplacePatternTest.java
@@ -0,0 +1,28 @@
+package com.juho.leetCode.mayLeetCodingChallenge.day21;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class FindAndReplacePatternTest {
+
+    FindAndReplacePattern frp = new FindAndReplacePattern();
+    
+    @Test
+    public void findAndReplacePattern01Test() {
+        String[] words01 = {"abc", "deq", "mee", "aqq", "dkd", "ccc"};
+        List<String> result01 = new ArrayList<>();
+        result01.add("mee");
+        result01.add("aqq");
+        assertEquals(result01, frp.findAndReplacePattern(words01, "abb"));
+        
+        String[] words02 = {"a", "b", "c"};
+        List<String> result02 = new ArrayList<>();
+        result02.add("a");
+        result02.add("b");
+        result02.add("c");
+        assertEquals(result02, frp.findAndReplacePattern(words02, "a"));
+    }
+}
