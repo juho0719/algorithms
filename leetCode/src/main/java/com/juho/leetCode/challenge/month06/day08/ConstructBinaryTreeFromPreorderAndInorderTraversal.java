1c62af98ee39ab51301456e3ebdd07ca86f3b10a
juho0719 <juho0719@naver.com>
2021-06-09 22:47:04 +0900

    JuneLeetCodingChallenge
    - day08 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month06/day08/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,30 @@
+package com.juho.leetCode.challenge.month06.day08;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.juho.leetCode.datastructure.TreeNode;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    int preorderIndex;
+    Map<Integer, Integer> inorderIndexMap;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preorderIndex = 0;
+        inorderIndexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndexMap.put(inorder[i], i);
+        }
+
+        return arrayToTree(preorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode arrayToTree(int[] preorder, int left, int right) {
+        if (left > right) return null;
+
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
+        root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
+        return root;
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month06/day08/ConstructBinaryTreeFromPreorderAndInorderTraversalTest.java
@@ -0,0 +1,29 @@
+package com.juho.leetCode.challenge.month06.day08;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.juho.leetCode.datastructure.TreeNode;
+
+import org.junit.jupiter.api.Test;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversalTest {
+
+    ConstructBinaryTreeFromPreorderAndInorderTraversal cbtpit = new ConstructBinaryTreeFromPreorderAndInorderTraversal();
+
+    @Test
+    public void constructBinaryTreeFromPreorderAndInorderTraversal01Test() {
+        int[] preorder01 = {3,9,20,15,7};
+        int[] inorder01 = {9,3,15,20,7};
+        TreeNode result101 = new TreeNode(3);
+        TreeNode result102 = new TreeNode(9);
+        TreeNode result103 = new TreeNode(20);
+        TreeNode result104 = new TreeNode(15);
+        TreeNode result105 = new TreeNode(7);
+        result101.left = result102;
+        result101.right = result103;
+        result103.left = result104;
+        result103.right = result105;
+        assertEquals(result101.toString(), cbtpit.buildTree(preorder01, inorder01).toString());
+    }
+
+}
