275954b4f8a14f224347a887f392de375f812ce1
juho0719 <juho0719@naver.com>
2021-06-19 17:52:49 +0900

    JuneLeetCodingChallenge
    - day18 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month06/day18/RangeSumQueryMutableTest.java
@@ -0,0 +1,18 @@
+package com.juho.leetCode.challenge.month06.day18;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class RangeSumQueryMutableTest {
+
+    @Test
+    public void rangeSumQueryMutable01Test() {
+        int[] nums01 = {1,3,5};
+        RangeSumQueryMutable rsqm = new RangeSumQueryMutable(nums01);
+        assertEquals(9, rsqm.sumRange(0, 2));
+
+        rsqm.update(1, 2);
+        assertEquals(8, rsqm.sumRange(0, 2));
+    }
+
+}
