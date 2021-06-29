16ee82ba958af69b9df67f58e4f36048d3c0867a
juho0719 <juho0719@naver.com>
2021-06-29 17:34:23 +0900

    JuneLeetCodingChallenge
    - day28 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month06/day28/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,15 @@
+package com.juho.leetCode.challenge.month06.day28;
+
+public class RemoveAllAdjacentDuplicatesInString {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
