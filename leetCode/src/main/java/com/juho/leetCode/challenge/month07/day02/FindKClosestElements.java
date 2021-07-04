8b75492202e2c29af02fc7e5ed4f4a5963734b9b
juho0719 <juho0719@naver.com>
2021-07-04 13:58:44 +0900

    JulyLeetCodingChallenge
    - day02 finished
    - day03 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month07/day02/FindKClosestElements.java
@@ -0,0 +1,27 @@
+package com.juho.leetCode.challenge.month07.day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindKClosestElements {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - k;
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        
+        return result;
+    }
+}
