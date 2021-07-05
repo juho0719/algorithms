9008323f9af10e56b01a08371002f0a80fb59fb8
juho0719 <juho0719@naver.com>
2021-07-05 23:51:32 +0900

    JulyLeetCodingChallenge
    - day04 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month07/day04/CountVowelsPermutationTest.java
@@ -0,0 +1,17 @@
+package com.juho.leetCode.challenge.month07.day04;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CountVowelsPermutationTest {
+
+    CountVowelsPermutation cvp = new CountVowelsPermutation();
+
+    @Test
+    public void countVowelsPermutation01Test() {
+        assertEquals(5, cvp.countVowelPermutation(1));
+        assertEquals(10, cvp.countVowelPermutation(2));
+        assertEquals(68, cvp.countVowelPermutation(5));
+    }
+
+}
