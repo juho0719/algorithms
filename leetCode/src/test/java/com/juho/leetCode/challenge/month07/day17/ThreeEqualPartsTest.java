c44aa2ae522db6f64cf27019a5cfcc3ee7287f19
juho0719 <juho0719@naver.com>
2021-07-18 11:39:28 +0900

    JulyLeetCodingChallenge
    - day16 finished
    - day17 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month07/day17/ThreeEqualPartsTest.java
@@ -0,0 +1,25 @@
+package com.juho.leetCode.challenge.month07.day17;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class ThreeEqualPartsTest {
+
+    ThreeEqualParts tep = new ThreeEqualParts();
+
+    @Test
+    public void threeEqualParts01Test() {
+        int[] arr01 = {1,0,1,0,1};
+        int[] result01 = {0,3};
+        assertArrayEquals(result01, tep.threeEqualParts(arr01));
+
+        int[] arr02 = {1,1,0,1,1};
+        int[] result02 = {-1,-1};
+        assertArrayEquals(result02, tep.threeEqualParts(arr02));
+
+        int[] arr03 = {1,1,0,0,1};
+        int[] result03 = {0,2};
+        assertArrayEquals(result03, tep.threeEqualParts(arr03));
+    }
+
+}
