37c183e110982186d4c8e9e38ed2fa82f4ea023a
juho0719 <juho0719@naver.com>
2021-08-04 14:50:15 +0900

    AugustLeetCodingChallenge
    - day03 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month08/day03/SubsetsII.java
@@ -0,0 +1,27 @@
+package com.juho.leetCode.challenge.month08.day03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+    List<List<Integer>> output;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort( nums ); 
+        output = new ArrayList<>();
+        backTracking( 0, nums, new ArrayList<>() );
+        return output;
+    }
+    
+    public void backTracking( int index, int[] nums, List<Integer> list ) {
+        output.add( new ArrayList<>(list) );
+        for( int i = index;i < nums.length; i++ )
+        {
+            if( i != index && nums[i] == nums[i-1] ) 
+                continue;
+            list.add( nums[i]); 
+            backTracking(i+1,nums,list);
+            list.remove( list.size()-1);
+        }
+    }
+}
