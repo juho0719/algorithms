6f1261b29834943546e180b86e21ec028c99fd0b
juho0719 <juho0719@naver.com>
2021-08-08 15:37:41 +0900

    AugustLeetCodingChallenge
    - day07 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month08/day07/PalindromePartitioningIITest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.challenge.month08.day07;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PalindromePartitioningIITest {
+
+    PalindromePartitioningII pp2 = new PalindromePartitioningII();
+
+    @Test
+    public void palindromePartitioningII01Test() {
+        String s01 = "aab";
+        assertEquals(1, pp2.minCut(s01));
+
+        String s02 = "a";
+        assertEquals(0, pp2.minCut(s02));
+
+        String s03 = "ab";
+        assertEquals(1, pp2.minCut(s03));
+    }
+}
