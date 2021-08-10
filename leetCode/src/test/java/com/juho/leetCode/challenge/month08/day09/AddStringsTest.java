4dfcdf376f96875d80929250665e0cebbcdd89fa
juho0719 <juho0719@naver.com>
2021-08-10 17:09:36 +0900

    AugustLeetCodingChallenge
    - day09 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month08/day09/AddStringsTest.java
@@ -0,0 +1,24 @@
+package com.juho.leetCode.challenge.month08.day09;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AddStringsTest {
+
+    AddStrings as = new AddStrings();
+
+    @Test
+    public void rankTransformOfaMatrix01Test() {
+        String num101 = "11";
+        String num102 = "123";
+        assertEquals("134", as.addStrings(num101, num102));
+
+        String num201 = "456";
+        String num202 = "77";
+        assertEquals("533", as.addStrings(num201, num202));
+
+        String num301 = "0";
+        String num302 = "0";
+        assertEquals("0", as.addStrings(num301, num302));
+    }
+}
