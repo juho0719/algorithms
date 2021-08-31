6c3eaecbe3f30b07938182d6b96e3b3efe6f9a18
juho0719 <juho0719@naver.com>
2021-08-31 13:44:17 +0900

    AugustLeetCodingChallenge
    - day29 finished
    - day30 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month08/day29/PatchingArrayTest.java
@@ -0,0 +1,24 @@
+package com.juho.leetCode.challenge.month08.day29;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PatchingArrayTest {
+
+    PatchingArray pa = new PatchingArray();
+
+    @Test
+    public void patchingArray01Test() {
+        int[] nums01 = {1,3};
+        int n01 = 6;
+        assertEquals(1, pa.minPatches(nums01, n01));
+
+        int[] nums02 = {1,5,10};
+        int n02 = 20;
+        assertEquals(2, pa.minPatches(nums02, n02));
+
+        int[] nums03 = {1,2,2};
+        int n03 = 5;
+        assertEquals(0, pa.minPatches(nums03, n03));
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month08/day30/RangeAdditionIITest.java
@@ -0,0 +1,22 @@
+package com.juho.leetCode.challenge.month08.day30;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class RangeAdditionIITest {
+
+    RangeAdditionII ra2 = new RangeAdditionII();
+
+    @Test
+    public void rangeAdditionII01Test() {
+        int m01 = 3;
+        int n01 = 3;
+        int[][] ops01 = {{2,2},{3,3}};
+        assertEquals(4, ra2.maxCount(m01, n01, ops01));
+
+        int m02 = 3;
+        int n02 = 3;
+        int[][] ops02 = {{2,2},{3,3},{3,3},{3,3},{2,2},{3,3},{3,3},{3,3},{2,2},{3,3},{3,3},{3,3}};
+        assertEquals(4, ra2.maxCount(m02, n02, ops02));
+    }
+}
