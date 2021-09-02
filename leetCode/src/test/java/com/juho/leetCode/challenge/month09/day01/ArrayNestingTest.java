81854759aa38f575d2cd85fd8fa84f8abe59e9dd
juho0719 <juho0719@naver.com>
2021-09-02 11:58:04 +0900

    SeptemberLeetCodingChallenge
    - day01 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month09/day01/ArrayNestingTest.java
@@ -0,0 +1,18 @@
+package com.juho.leetCode.challenge.month09.day01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ArrayNestingTest {
+
+    ArrayNesting an = new ArrayNesting();
+
+    @Test
+    public void arrayNesting01Test() {
+        int[] nums01 = {5,4,0,3,1,6,2};
+        assertEquals(4, an.arrayNesting(nums01));
+
+        int[] nums02 = {0,1,2};
+        assertEquals(1, an.arrayNesting(nums02));
+    }
+}
