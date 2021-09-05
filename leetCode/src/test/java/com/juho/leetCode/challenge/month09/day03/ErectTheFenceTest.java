b3361e46c7b88c78d7e366a091a00701a7f987d6
juho0719 <juho0719@naver.com>
2021-09-05 15:10:53 +0900

    SeptemberLeetCodingChallenge
    - day03 finished
    - day04 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month09/day03/ErectTheFenceTest.java
@@ -0,0 +1,13 @@
+package com.juho.leetCode.challenge.month09.day03;
+
+import org.junit.jupiter.api.Test;
+
+public class ErectTheFenceTest {
+
+    ErectTheFence ef = new ErectTheFence();
+
+    @Test
+    public void erectTheFence01Test() {
+        //skip
+    }
+}
