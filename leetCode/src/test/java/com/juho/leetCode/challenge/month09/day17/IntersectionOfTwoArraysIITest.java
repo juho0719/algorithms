c60e8794af913ee76eacef7ef07af8f8126289e7
juho0719 <juho0719@naver.com>
2021-09-18 15:49:56 +0900

    SeptemberLeetCodingChallenge
    - day17 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month09/day17/IntersectionOfTwoArraysIITest.java
@@ -0,0 +1,18 @@
+package com.juho.leetCode.challenge.month09.day17;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IntersectionOfTwoArraysIITest {
+
+    IntersectionOfTwoArraysII sm = new IntersectionOfTwoArraysII();
+
+    @Test
+    public void intersectionOfTwoArraysII01Test() {
+        int[] nums101 = {1,2,2,1};
+        int[] nums102 = {2,2};
+        int[] result100 = {2,2};
+        assertArrayEquals(result100, sm.intersect(nums101, nums102));
+    }
+}
