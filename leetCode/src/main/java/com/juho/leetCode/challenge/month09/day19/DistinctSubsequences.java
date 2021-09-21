9ce4c2aed158efc773843285a54ad30ca2d0a62f
juho0719 <juho0719@naver.com>
2021-09-21 17:29:27 +0900

    SeptemberLeetCodingChallenge
    - day19 finished
    - day20 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month09/day19/DistinctSubsequences.java
@@ -0,0 +1,16 @@
+package com.juho.leetCode.challenge.month09.day19;
+
+public class DistinctSubsequences {
+    public int numDistinct(String s, String t) {
+        if(t.length()>s.length()) return 0;
+        int[][] result = new int[t.length()+1][s.length()+1];
+        for(int j=0; j<=s.length(); j++) result[0][j] = 1;
+        for(int i=0; i<t.length(); i++) {
+            for(int j=0; j<s.length(); j++) {
+                if(t.charAt(i) == s.charAt(j))  result[i+1][j+1] = result[i][j] + result[i+1][j];
+                else    result[i+1][j+1] = result[i+1][j];
+            }  
+        }
+        return result[t.length()][s.length()];
+    }
+}
