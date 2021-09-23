0295763ea536f34c166a4c70c9ef3cc1f79f860d
juho0719 <juho0719@naver.com>
2021-09-23 12:14:18 +0900

    SeptemberLeetCodingChallenge
    - day22 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month09/day22/MaximumLengthOfaConcatenatedStringWithUniqueCharacters.java
@@ -0,0 +1,48 @@
+package com.juho.leetCode.challenge.month09.day22;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MaximumLengthOfaConcatenatedStringWithUniqueCharacters {
+    public int maxLength(List<String> arr) {
+        Set<Integer> optSet = new HashSet<>();
+        for (String word : arr) 
+            wordToBitSet(optSet, word);
+        
+        int[] optArr = new int[optSet.size()];
+        int i = 0;
+        for (Integer num : optSet)
+            optArr[i++] = num;
+        return dfs(optArr, 0, 0);
+    }
+    
+    private void wordToBitSet(Set<Integer> optSet, String word) {
+        int charBitSet = 0;
+        for (char c : word.toCharArray()) {
+            int mask = 1 << c - 'a';
+            if ((charBitSet & mask) > 0)
+                return;
+            charBitSet += mask;
+        }
+        optSet.add(charBitSet + (word.length() << 26));
+    }
+    
+    private int dfs(int[] optArr, int pos, int res) {
+        int oldChars = res & ((1 << 26) - 1);
+        int oldLen = res >> 26;
+        int best = oldLen;
+        
+        for (int i = pos; i < optArr.length; i++) {
+            int newChars = optArr[i] & ((1 << 26) - 1);
+            int newLen = optArr[i] >> 26;
+            
+            if ((newChars & oldChars) != 0)
+                continue;
+            
+            int newRes = newChars + oldChars + (newLen + oldLen << 26);
+            best = Math.max(best, dfs(optArr, i + 1, newRes));
+        }
+        return best;
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month09/day22/MaximumLengthOfaConcatenatedStringWithUniqueCharactersTest.java
@@ -0,0 +1,33 @@
+package com.juho.leetCode.challenge.month09.day22;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class MaximumLengthOfaConcatenatedStringWithUniqueCharactersTest {
+
+    MaximumLengthOfaConcatenatedStringWithUniqueCharacters mlcsuc = new MaximumLengthOfaConcatenatedStringWithUniqueCharacters();
+
+    @Test
+    public void maximumLengthOfaConcatenatedStringWithUniqueCharacters01Test() {
+        List<String> arr01 = new ArrayList<>();
+        arr01.add("un");
+        arr01.add("iq");
+        arr01.add("ue");
+        assertEquals(4, mlcsuc.maxLength(arr01));
+
+        List<String> arr02 = new ArrayList<>();
+        arr02.add("cha");
+        arr02.add("r");
+        arr02.add("act");
+        arr02.add("ers");
+        assertEquals(6, mlcsuc.maxLength(arr02));
+
+        List<String> arr03 = new ArrayList<>();
+        arr03.add("abcdefghijklmnopqrstuvwxyz");
+        assertEquals(26, mlcsuc.maxLength(arr03));
+    }
+}
