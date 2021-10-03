5b7560b950dfc47ab016891e43b5d1988bdcac5e
juho0719 <juho0719@naver.com>
2021-10-03 18:31:48 +0900

    OctoberLeetCodingChallenge
    - day03 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month10/day03/JumpGameTest.java
@@ -0,0 +1,18 @@
+package com.juho.leetCode.challenge.month10.day03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class JumpGameTest {
+
+    JumpGame jg = new JumpGame();
+
+    @Test
+    public void dungeonGame01Test() {
+        int[] nums01 = {2,3,1,1,4};
+        assertEquals(true, jg.canJump(nums01));
+
+        int[] nums02 = {3,2,1,0,4};
+        assertEquals(false, jg.canJump(nums02));
+    }
+}
