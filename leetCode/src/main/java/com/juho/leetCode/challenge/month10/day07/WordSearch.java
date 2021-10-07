91cdb27cc46e6f6ae1cd1d5343ce77495ceffc39
juho0719 <juho0719@naver.com>
2021-10-07 13:15:42 +0900

    OctoberLeetCodingChallenge
    - day07 finished

---
+++ leetCode/src/main/java/com/juho/leetCode/challenge/month10/day07/WordSearch.java
@@ -0,0 +1,28 @@
+package com.juho.leetCode.challenge.month10.day07;
+
+public class WordSearch {
+    public boolean helper(int i ,int j,char board[][],String word) {
+        if(word.length() == 0)return true;
+        if(i<0 || j<0 || i>=board.length || j>= board[i].length || board[i][j]!=word.charAt(0))
+            return false;
+        
+        board[i][j] = ' ';
+        boolean top = helper(i-1,j,board,word.substring(1));
+        boolean bottom = helper(i+1,j,board,word.substring(1));
+        boolean left = helper(i,j-1,board,word.substring(1));
+        boolean right = helper(i,j+1,board,word.substring(1));
+        
+        board[i][j] = word.charAt(0);
+        return top || bottom || left || right;
+
+    }
+    public boolean exist(char[][] board, String word) {
+        for(int i =0;i<board.length;i++) {
+            for(int j =0;j<board[i].length;j++)
+            {
+                if(board[i][j] == word.charAt(0) && helper(i,j,board,word)) return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month10/day07/WordSearchTest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.challenge.month10.day07;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class WordSearchTest {
+
+    WordSearch ws = new WordSearch();
+
+    @Test
+    public void wordSearch01Test() {
+        char[][] board01 = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word01 = "ABCCED";
+        assertEquals(true, ws.exist(board01, word01));
+
+        char[][] board02 = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word02 = "ABCB";
+        assertEquals(false, ws.exist(board02, word02));
+    }
+}
