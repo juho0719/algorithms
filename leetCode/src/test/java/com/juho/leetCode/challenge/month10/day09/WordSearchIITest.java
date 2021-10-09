d6b51400b95e90775ec36d13fe22cd36633744b1
juho0719 <juho0719@naver.com>
2021-10-09 14:32:51 +0900

    OctoberLeetCodingChallenge
    - day08 finished
    - day09 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month10/day09/WordSearchIITest.java
@@ -0,0 +1,28 @@
+package com.juho.leetCode.challenge.month10.day09;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class WordSearchIITest {
+
+    WordSearchII ws = new WordSearchII();
+
+    @Test
+    public void wordSearchII01Test() {
+        char[][] board01 = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
+        String[] words01 = {"oath","pea","eat","rain"};
+        List<String> result01 = new ArrayList<>();
+        result01.add("oath");
+        result01.add("eat");
+        assertEquals(result01, ws.findWords(board01, words01));
+
+        char[][] board02 = {{'a','b'},{'c','d'}};
+        String[] words02 = {"abcd"};
+        List<String> result02 = new ArrayList<>();
+        assertEquals(result02, ws.findWords(board02, words02));
+    }
+}
