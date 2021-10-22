99099ba0d05ad302a663b8c4bd252bc317e44261
juho0719 <juho0719@naver.com>
2021-10-22 10:53:47 +0900

    OctoberLeetCodingChallenge
    - day22 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month10/day22/SortCharactersByFrequencyTest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.challenge.month10.day22;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SortCharactersByFrequencyTest {
+
+    SortCharactersByFrequency scf = new SortCharactersByFrequency();
+
+    @Test
+    public void sortCharactersByFrequency01Test() {
+        String s01 = "tree";
+        assertEquals("eert", scf.frequencySort(s01));
+
+        String s02 = "cccaaa";
+        assertEquals("aaaccc", scf.frequencySort(s02));
+
+        String s03 = "Aabb";
+        assertEquals("bbAa", scf.frequencySort(s03));
+    }
+}
