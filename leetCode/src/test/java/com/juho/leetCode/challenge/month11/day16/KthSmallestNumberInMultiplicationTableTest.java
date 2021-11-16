0d5d05befd55b069fe61d1b5700e695b7addda65
juho0719 <juho0719@naver.com>
2021-11-16 09:41:13 +0900

    NovemberLeetCodingChallenge
    - day16 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month11/day16/KthSmallestNumberInMultiplicationTableTest.java
@@ -0,0 +1,22 @@
+package com.juho.leetCode.challenge.month11.day16;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class KthSmallestNumberInMultiplicationTableTest {
+
+    KthSmallestNumberInMultiplicationTable ksnmt = new KthSmallestNumberInMultiplicationTable();
+
+    @Test
+    public void kthSmallestNumberInMultiplicationTable01Test() {
+        int m01 = 3;
+        int n01 = 3;
+        int k01 = 5;
+        assertEquals(3, ksnmt.findKthNumber(m01, n01, k01));
+
+        int m02 = 2;
+        int n02 = 3;
+        int k02 = 6;
+        assertEquals(6, ksnmt.findKthNumber(m02, n02, k02));
+    }
+}
