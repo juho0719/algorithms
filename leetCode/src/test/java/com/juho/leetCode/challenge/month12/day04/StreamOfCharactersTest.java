3599362340f263e9cbb05edb8321459e037af14a
juho0719 <juho0719@naver.com>
2021-12-04 15:42:47 +0900

    DecemberLeetCodingChallenge
    - day03 finished
    - day04 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month12/day04/StreamOfCharactersTest.java
@@ -0,0 +1,25 @@
+package com.juho.leetCode.challenge.month12.day04;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class StreamOfCharactersTest {
+
+    @Test
+    public void streamOfCharacters01Test() {
+        String[] words = {"cd", "f", "kl"};
+        StreamOfCharacters streamChecker = new StreamOfCharacters(words);
+        assertEquals(false, streamChecker.query('a'));
+        assertEquals(false, streamChecker.query('b'));
+        assertEquals(false, streamChecker.query('c'));
+        assertEquals(true, streamChecker.query('d'));
+        assertEquals(false, streamChecker.query('e'));
+        assertEquals(true, streamChecker.query('f'));
+        assertEquals(false, streamChecker.query('g'));
+        assertEquals(false, streamChecker.query('h'));
+        assertEquals(false, streamChecker.query('i'));
+        assertEquals(false, streamChecker.query('j'));
+        assertEquals(false, streamChecker.query('k'));
+        assertEquals(true, streamChecker.query('l'));
+    }
+}
