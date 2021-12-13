148ff54a45c07ab02100dd8217da1d79977d4520
juho0719 <juho0719@naver.com>
2021-12-13 09:43:53 +0900

    DecemberLeetCodingChallenge
    - day13 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month12/day13/ConsecutiveCharactersTest.java
@@ -0,0 +1,27 @@
+package com.juho.leetCode.challenge.month12.day13;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ConsecutiveCharactersTest {
+
+    ConsecutiveCharacters cc = new ConsecutiveCharacters();
+
+    @Test
+    public void consecutiveCharacters01Test() {
+        String s01 = "leetcode";
+        assertEquals(2, cc.maxPower(s01));
+
+        String s02 = "abbcccddddeeeeedcba";
+        assertEquals(5, cc.maxPower(s02));
+
+        String s03 = "triplepillooooow";
+        assertEquals(5, cc.maxPower(s03));
+
+        String s04 = "hooraaaaaaaaaaay";
+        assertEquals(11, cc.maxPower(s04));
+
+        String s05 = "tourist";
+        assertEquals(1, cc.maxPower(s05));
+    }
+}
