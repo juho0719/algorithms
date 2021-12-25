8ee401c0b00336d76ffadf4a8b779b93efa95d12
juho0719 <juho0719@naver.com>
2021-12-25 12:04:18 +0900

    DecemberLeetCodingChallenge
    - day25 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/month12/day25/BasicCalculatorIITest.java
@@ -0,0 +1,21 @@
+package com.juho.leetCode.challenge.month12.day25;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class BasicCalculatorIITest {
+
+    BasicCalculatorII bc2 = new BasicCalculatorII();
+
+    @Test
+    public void basicCalculatorII01Test() {
+        String s01 = "3+2*2";
+        assertEquals(7, bc2.calculate(s01));
+
+        String s02 = " 3/2 ";
+        assertEquals(1, bc2.calculate(s02));
+
+        String s03 = " 3+5 / 2 ";
+        assertEquals(5, bc2.calculate(s03));
+    }
+}
