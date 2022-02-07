88c060c658813dc3b71f0d3a67a4e19a4b2bc04f
juho0719 <juho0719@naver.com>
2022-02-07 23:59:48 +0900

    d

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/y2022/month02/day07/FindTheDifferenceTest.java
@@ -0,0 +1,14 @@
+package com.juho.leetCode.challenge.y2022.month02.day07;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class FindTheDifferenceTest {
+
+    FindTheDifference fd = new FindTheDifference();
+
+    @Test
+    public void findTheDifference01Test() {
+        
+    }
+}
