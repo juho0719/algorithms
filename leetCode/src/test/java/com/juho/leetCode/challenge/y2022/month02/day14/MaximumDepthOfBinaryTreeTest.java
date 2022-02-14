70b384c8db79d396746203d9800782a5f4ae8545
juho0719 <juho0719@naver.com>
2022-02-14 23:29:25 +0900

    FebruaryLeetCodingChallenge
    - day14 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/y2022/month02/day14/MaximumDepthOfBinaryTreeTest.java
@@ -0,0 +1,26 @@
+package com.juho.leetCode.challenge.y2022.month02.day14;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.juho.leetCode.datastructure.TreeNode;
+
+import org.junit.jupiter.api.Test;
+
+public class MaximumDepthOfBinaryTreeTest {
+
+    MaximumDepthOfBinaryTree mdbt = new MaximumDepthOfBinaryTree();
+
+    @Test
+    public void maximumDepthOfBinaryTree01Test() {
+        TreeNode root101 = new TreeNode(3);
+        TreeNode root102 = new TreeNode(9);
+        TreeNode root103 = new TreeNode(20);
+        TreeNode root104 = new TreeNode(15);
+        TreeNode root105 = new TreeNode(7);
+        root101.left = root102;
+        root101.right = root103;
+        root103.left = root104;
+        root103.right = root105;
+        assertEquals(3, mdbt.maxDepth(root101));
+    }
+}
