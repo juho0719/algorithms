bbc21d629ca9dc75c20e183158e257fd1d6e60f5
juho0719 <juho0719@naver.com>
2022-06-06 19:54:58 +0900

    JuneLeetCodingChallenge
    - day06 finished

---
+++ leetCode/src/test/java/com/juho/leetCode/challenge/y2022/month06/day06/IntersectionOfTwoLinkedListsTest.java
@@ -0,0 +1,13 @@
+package com.juho.leetCode.challenge.y2022.month06.day06;
+
+import org.junit.jupiter.api.Test;
+
+public class IntersectionOfTwoLinkedListsTest {
+
+    IntersectionOfTwoLinkedLists itl = new IntersectionOfTwoLinkedLists();
+
+    @Test
+    public void intersectionOfTwoLinkedLists01Test() {
+        // skip
+    }
+}
